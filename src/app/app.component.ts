a20e80b4006af8f323a9a66325a7361f46979200
Aleksandar Tsolov <35367857+A-Tsolov@users.noreply.github.com>
2019-11-25 01:01:57 +0000

    Drew alcohol data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,12 +14,11 @@ import SampleJson from 'data.json';
 })
 export class AppComponent {
   title = 'GitFit';
-  public lineChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
-    { data: [180, 480, 770, 90, 1000, 270, 400], label: 'Series C', yAxisID: 'y-axis-1' }
-  ];
-  public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  chartData;
+  countries;
+  years;
+  public lineChartData: ChartDataSets[];
+  public lineChartLabels: Label[];
   public lineChartOptions: (ChartOptions & { annotation: any }) = {
     responsive: true,
     scales: {
@@ -30,34 +29,7 @@ export class AppComponent {
           id: 'y-axis-0',
           position: 'left',
         },
-        {
-          id: 'y-axis-1',
-          position: 'right',
-          gridLines: {
-            color: 'rgba(255,0,0,0.3)',
-          },
-          ticks: {
-            fontColor: 'red',
-          }
-        }
       ]
-    },
-    annotation: {
-      annotations: [
-        {
-          type: 'line',
-          mode: 'vertical',
-          scaleID: 'x-axis-0',
-          value: 'March',
-          borderColor: 'orange',
-          borderWidth: 2,
-          label: {
-            enabled: true,
-            fontColor: 'orange',
-            content: 'LineAnno'
-          }
-        },
-      ],
     },
   };
   public lineChartColors: Color[] = [
@@ -91,100 +63,44 @@ export class AppComponent {
   public lineChartPlugins = [pluginAnnotations];
 
   @ViewChild(BaseChartDirective, { static: true }) chart: BaseChartDirective;
+  constructor() {
 
-  //   public lineChartData: ChartDataSets[] = [
-  //   { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-  //   { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
-  //   { data: [180, 480, 770, 90, 1000, 270, 400], label: 'Series C', yAxisID: 'y-axis-1' }
-  // ];
-  // public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  // public lineChartOptions: (ChartOptions & { annotation: any }) = {
-  //   responsive: true,
-  //   scales: {
-  //     // We use this empty structure as a placeholder for dynamic theming.
-  //     xAxes: [{}],
-  //     yAxes: [
-  //       {
-  //         id: 'y-axis-0',
-  //         position: 'left',
-  //       },
-  //       {
-  //         id: 'y-axis-1',
-  //         position: 'right',
-  //         gridLines: {
-  //           color: 'rgba(255,0,0,0.3)',
-  //         },
-  //         ticks: {
-  //           fontColor: 'red',
-  //         }
-  //       }
-  //     ]
-  //   },
-  //   annotation: {
-  //     annotations: [
-  //       {
-  //         type: 'line',
-  //         mode: 'vertical',
-  //         scaleID: 'x-axis-0',
-  //         value: 'March',
-  //         borderColor: 'orange',
-  //         borderWidth: 2,
-  //         label: {
-  //           enabled: true,
-  //           fontColor: 'orange',
-  //           content: 'LineAnno'
-  //         }
-  //       },
-  //     ],
-  //   },
-  // };
-  // public lineChartColors: Color[] = [
-  //   { // grey
-  //     backgroundColor: 'rgba(148,159,177,0.2)',
-  //     borderColor: 'rgba(148,159,177,1)',
-  //     pointBackgroundColor: 'rgba(148,159,177,1)',
-  //     pointBorderColor: '#fff',
-  //     pointHoverBackgroundColor: '#fff',
-  //     pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-  //   },
-  //   { // dark grey
-  //     backgroundColor: 'rgba(77,83,96,0.2)',
-  //     borderColor: 'rgba(77,83,96,1)',
-  //     pointBackgroundColor: 'rgba(77,83,96,1)',
-  //     pointBorderColor: '#fff',
-  //     pointHoverBackgroundColor: '#fff',
-  //     pointHoverBorderColor: 'rgba(77,83,96,1)'
-  //   },
-  //   { // red
-  //     backgroundColor: 'rgba(255,0,0,0.3)',
-  //     borderColor: 'red',
-  //     pointBackgroundColor: 'rgba(148,159,177,1)',
-  //     pointBorderColor: '#fff',
-  //     pointHoverBackgroundColor: '#fff',
-  //     pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-  //   }
-  // ];
-  // public lineChartLegend = true;
-  // public lineChartType = 'line';
-  // public lineChartPlugins = [pluginAnnotations];
-
-  // @ViewChild(BaseChartDirective, { static: true }) chart: BaseChartDirective;
-
-  constructor() { 
-    
   }
 
   ngOnInit() {
-    console.log(SampleJson);
+    // console.log(SampleJson);
+    let data = SampleJson
+    let new_data={}
+    console.log(data);
+    for (let entry of SampleJson["fact"]) {
+      if (!new_data[entry["dims"]["COUNTRY"]]){
+        new_data[entry["dims"]["COUNTRY"]]={"Years":[], "Values":[]}
+      }
+      if(entry["dims"]["ALCOHOLTYPE"]==="All types"){
+        new_data[entry["dims"]["COUNTRY"]]["Years"].push(entry["dims"]["YEAR"])
+        new_data[entry["dims"]["COUNTRY"]]["Values"].push(entry["Value"])
+      }
+    }
+    console.log(new_data);
+    this.countries=Object.keys(new_data);
+    this.chartData=[]
+    for (let country of this.countries) {
+      let chartEntry={"data": new_data[country]["Values"].reverse().map(function(item) {return parseFloat(item);}), "label": country}
+      this.chartData.push(chartEntry)
+    }
+    this.years = new_data["Albania"]["Years"].reverse();
+    this.lineChartData=this.chartData.slice(0,10)
+    this.lineChartLabels=this.years;
   }
 
   public randomize(): void {
-    for (let i = 0; i < this.lineChartData.length; i++) {
-      for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-        this.lineChartData[i].data[j] = this.generateNumber(i);
-      }
-    }
-    this.chart.update();
+    console.log(this.lineChartData);
+    // for (let i = 0; i < this.lineChartData.length; i++) {
+    //   for (let j = 0; j < this.lineChartData[i].data.length; j++) {
+    //     this.lineChartData[i].data[j] = this.generateNumber(i);
+    //   }
+    // }
+    // this.chart.update();
   }
 
   private generateNumber(i: number) {
