1274b10d8e64eaa25dd8451fe43a088b0dde2f28
anakimkn@abv.bg <anakimkn@abv.bg>
2019-12-04 22:04:35 +0000

    All goals added

--- src/app/recommendation/recommendation.component.ts
+++ src/app/recommendation/recommendation.component.ts
@@ -7,24 +7,60 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-recommendation',
   templateUrl: './recommendation.component.html',
-  styleUrls: ['./recommendation.component.css']
+  styleUrls: ['./recommendation.component.sass']
 })
 export class RecommendationComponent implements OnInit {
 
-  constructor(private recService: RecommendationService) { }
+  constructor(private recService: RecommendationService) {}
+
   user = null;
+
+  goalsObj = {
+    bmiGoal: 0,
+    alcConsGoal: 0,
+    cigUseGoal : 0
+  };
 
   BMI = 0;
   bmiString = '';
-  bmiGoals = [];
+  bmiGoal = 0;
+
+  bmiPlus025 = false;
+  bmiPlus05 = false;
+  bmiKeep = false;
+  bmiMinus025 = false;
+  bmiMinus05 = false;
+
 
   alcCons = 0;
   alcConsString = '';
-  alcConsGoals = [];
+  alcConsGoal = 0;
+
+  alcConsKeep0 = false;
+  alcConsKeepUnder14 = false;
+  alcConsReduce5 = false;
+  alcConsReduce10 = false;
 
   cigUse = 0;
   cigUseString = '';
-  cigUseGoals = [];
+  cigUseGoal = 0;
+
+  cigUseReduce5 = false;
+  cigUseReduce10 = false;
+  cigUseKeep0 = false;
+
+  underweight = false;
+  normalweight = false;
+  overweight = false;
+  obese = false;
+
+  normalDrinker = false;
+  heavyDrinker = false;
+
+  nonSmoker = false;
+  smoker = false;
+
+
 
   doStuff() {
     this.processAlcCons();
@@ -34,36 +70,57 @@ export class RecommendationComponent implements OnInit {
 
   processBMI() {
     this.BMI = this.user.weight / (this.user.height / 100) ** 2;
-    if (this.BMI < constants.BMI.NORMAL_LOWER_LIMIT) {
-        this.bmiString = 'You are underweight! Eat some Kettle ;).';
-    } else if (this.BMI < constants.BMI.OVERWEIGHT_LOWER_LIMIT) {
-      this.bmiString = 'Yer fine.';
-    } else if (this.BMI < constants.BMI.OBESE_LOWER_LIMIT) {
+
+    this.underweight = this.BMI < constants.BMI.NORMAL_LOWER_LIMIT;
+    this.normalweight = this.BMI > constants.BMI.NORMAL_LOWER_LIMIT && this.BMI < constants.BMI.OVERWEIGHT_LOWER_LIMIT;
+    this.overweight = this.BMI > constants.BMI.OVERWEIGHT_LOWER_LIMIT && this.BMI < constants.BMI.OBESE_LOWER_LIMIT;
+    this.obese = this.BMI > constants.BMI.OBESE_LOWER_LIMIT;
+
+    if (this.underweight) {
+        this.bmiString = 'You are underweight.';
+        // + 0,25 or 0,5
+    } else if (this.normalweight) {
+      this.bmiString = 'You are at an average weight';
+      // 1 goal keep weight
+    } else if (this.overweight) {
       this.bmiString = 'Lose some weight.';
+      // - 0,25 or 0.5
     } else {
       this.bmiString = 'Damn ya fat.';
+      // - 0,25 or 0.5
+
     }
   }
 
   processAlcCons() {
     this.alcCons = this.user.alcCons;
-    if (this.alcCons < constants.ALCOHOL.WEEKLY_UNIT_MAX_LIMIT) {
+
+    this.normalDrinker = this.alcCons <= constants.ALCOHOL.WEEKLY_UNIT_MAX_LIMIT;
+    this.heavyDrinker = this.alcCons > constants.ALCOHOL.WEEKLY_UNIT_MAX_LIMIT;
+
+
+    if (this.normalDrinker) {
       this.alcConsString = 'Your alcohol consumption is within the recommended limit.';
-      //
+      // 0 or 14
     } else {
       this.alcConsString = 'Your alcohol consumption is over the recommended limit.';
-      //
+      // try to reduce by 5 %, if 5% < 1, then reduce by 1. Until you stop. 5 or 10
+
     }
   }
 
   processCigUse() {
     this.cigUse = this.user.cigUse;
-    if (this.cigUse >= constants.CIGARETTES.UNHEALTHY_LOWER_LIMIT) {
+
+    this.smoker = this.cigUse >= constants.CIGARETTES.UNHEALTHY_LOWER_LIMIT;
+    this.nonSmoker = this.cigUse < constants.CIGARETTES.UNHEALTHY_LOWER_LIMIT;
+
+    if (this.smoker) {
       this.cigUseString = 'You smoke, STAHP.';
-      // try to reduce by 5 %, if 5% < 1, then reduce by 1. Until you stop.
+      // try to reduce by 5 %, if 5% < 1, then reduce by 1. Until you stop. 5 or 10
     } else {
       this.cigUseString = 'GOOD BOI.';
-      // Keep at it.
+      // Keep at it. Considered non - smoker
     }
   }
 
