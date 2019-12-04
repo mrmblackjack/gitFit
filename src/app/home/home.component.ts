394f615600f30550e1074c9ff53cecc0e69b7ec4
tanyaKk97 <2248916k@student.gla.ac.uk>
2019-12-04 22:17:00 +0000

    diary

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { SignInserviceService } from './../sign-inservice.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private avbarService:SignInserviceService) {}
+  username = "User 1";
 
   ngOnInit() {
+    this.avbarService.changeNavbarVis(true);
+
   }
+
 
 }
